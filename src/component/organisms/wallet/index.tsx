1c1a01f37de7fd8a7191ad767a7cc7ff2e6fdf29
vin <olivin_esguerra@yahoo.com>
2025-01-31 23:17:19 +0800

    show wallet balance

--- src/component/organisms/wallet/index.tsx
+++ src/component/organisms/wallet/index.tsx
@@ -1,7 +1,12 @@
 
 import { MetaMaskSDK } from "@metamask/sdk";
-import { Key } from "react";
-import { useAccount, useConnect, useDisconnect } from "wagmi"
+import { Key, useEffect } from "react";
+import { 
+    useAccount, 
+    useConnect, 
+    useDisconnect,
+    useReadContract
+} from "wagmi"
 
 const MMSDK = new MetaMaskSDK({
     dappMetadata: {
@@ -13,33 +18,66 @@ const MMSDK = new MetaMaskSDK({
 
 interface IWalletProps {};
 export const Wallet = (props: IWalletProps) => {
-    const { address, isConnected } = useAccount();
+    const { address, isConnected, chainId, chain, connector, isReconnecting, status } = useAccount();
     const { connectors, connect, isPending } = useConnect();
     const { disconnect } = useDisconnect();
+    
+    const { 
+        data: balance,
+        isError,
+        isLoading 
+      } = useReadContract({
+        address: address,
+        abi: [
+          {
+            name: "balanceOf",
+            type: "function",
+            stateMutability: "view",
+            inputs: [{ name: "owner", type: "address" }],
+            outputs: [{ name: "balance", type: "uint256" }],
+          },
+        ],
+        functionName: "balanceOf",
+        args: ["0x03A71968491d55603FFe1b11A9e23eF013f75bCF"],
+    });
+
+    useEffect(() =>{
+        const getUserAccount = async () => {
+            const acc = await connector?.getAccounts();
+            return acc;
+        };
+
+        if (isConnected) {
+            getUserAccount();
+        }
+    },[isConnected]);
 
     if (isConnected) {
         return (
             <div className="flex grow p-[20px] overflow-y-clip bg-gray-700 justify-center items-center flex-col">
                 <img className="flex h-[200px]" src={require('../../../assets/metamask.svg')} />
                 <div className="flex text-white">Connected to {address}</div>
+                <div className="flex text-white">Balance: {balance?.toString() || 0.00}</div>
                 <button className="flex text-white" onClick={() => disconnect()}>Disconnect</button>
             </div>
-        )
+        );
     }
-    
+ 
     return (
         <div className="flex grow p-[20px] overflow-y-clip bg-gray-700 justify-center items-center flex-col">
             <img className="flex h-[200px]" src={require('../../../assets/metamask.svg')} />
-            {connectors.map((connector: { uid: Key | null | undefined; name: any; }) => (
-                <button
-                    key={connector.uid}
-                    onClick={() => connect({ connector })}
-                    disabled={isPending}
-                    className="flex text-white"
-                >
-                    {isPending ? "Connecting..." : `Connect ${connector.name}`}
-                </button>
-            ))}
+            {connectors.map((connector: { uid: Key | null | undefined; name: any; }) => {
+                return (
+                    <button
+                        key={connector.uid}
+                        onClick={() => connect({ connector })}
+                        disabled={isPending}
+                        className="flex text-white"
+                    >
+                        {isPending ? "Connecting..." : `Connect ${connector.name}`}
+                    </button>
+                );
+            })}
         </div>
     );
 }
