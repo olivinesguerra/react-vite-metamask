fb33e9169c6a53a3b267764cee95d303aac8826b
vin <olivin_esguerra@yahoo.com>
2025-01-31 22:19:53 +0800

    connect metamask

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import './App.css'
+import './App.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { http, WagmiProvider, createConfig } from "wagmi";
+import { mainnet, linea, lineaSepolia } from "wagmi/chains";
+import { metaMask } from "wagmi/connectors";
+
+const config = createConfig({
+  ssr: true, // Make sure to enable this for server-side rendering (SSR) applications.
+  chains: [mainnet, linea, lineaSepolia],
+  connectors: [metaMask()],
+  transports: {
+    [mainnet.id]: http(),
+    [linea.id]: http(),
+    [lineaSepolia.id]: http(),
+  },
+});
 
 function App() {
+
+  const client = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <WagmiProvider config={config}>
+       <QueryClientProvider client={client}>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+       </QueryClientProvider>
+    </WagmiProvider>
+
   )
 }
 
--- src/component/organisms/wallet/index.tsx
+++ src/component/organisms/wallet/index.tsx
@@ -1,5 +1,7 @@
 
 import { MetaMaskSDK } from "@metamask/sdk";
+import { Key } from "react";
+import { useAccount, useConnect, useDisconnect } from "wagmi"
 
 const MMSDK = new MetaMaskSDK({
     dappMetadata: {
@@ -11,9 +13,33 @@ const MMSDK = new MetaMaskSDK({
 
 interface IWalletProps {};
 export const Wallet = (props: IWalletProps) => {
+    const { address, isConnected } = useAccount();
+    const { connectors, connect, isPending } = useConnect();
+    const { disconnect } = useDisconnect();
+
+    if (isConnected) {
+        return (
+            <div className="flex grow p-[20px] overflow-y-clip bg-gray-700 justify-center items-center flex-col">
+                <img className="flex h-[200px]" src={require('../../../assets/metamask.svg')} />
+                <div className="flex text-white">Connected to {address}</div>
+                <button className="flex text-white" onClick={() => disconnect()}>Disconnect</button>
+            </div>
+        )
+    }
+    
     return (
-        <div className="flex grow w-full h-full bg-orange-500">
-            
+        <div className="flex grow p-[20px] overflow-y-clip bg-gray-700 justify-center items-center flex-col">
+            <img className="flex h-[200px]" src={require('../../../assets/metamask.svg')} />
+            {connectors.map((connector: { uid: Key | null | undefined; name: any; }) => (
+                <button
+                    key={connector.uid}
+                    onClick={() => connect({ connector })}
+                    disabled={isPending}
+                    className="flex text-white"
+                >
+                    {isPending ? "Connecting..." : `Connect ${connector.name}`}
+                </button>
+            ))}
         </div>
     );
 }
